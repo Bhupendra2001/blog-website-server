2699b427f3b719bb41300c4c7752d8e9a1e333f0
Bhupendra2001 <bhupendran42@gmail.com>
2023-01-04 22:36:03 +0530

    assignment 1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import './App.css'
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      input :'',
+      showPara : false,
+    }
+  };
+
+  render(){
+
+    return (
+      <div>
+        <textarea
+        value={this.state.input}
+        onChange={(e)=> this.setState({input : e.target.value})}/>
+        <br/>
+        <button
+        onClick={()=>{
+          this.setState({showPara : !this.state.showPara})
+        }} > Display in UpperCase</button>
+        {this.state.showPara && <p id="up">{this.state.input.toUpperCase()}</p>}
+      </div>
+    )
+  }
 }
 
 export default App;
