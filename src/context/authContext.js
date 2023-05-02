2f9cb609adc4fda353e5a9a08aaa86a99280b58f
bhupendra_namedev <115540810+Bhupendra2001@users.noreply.github.com>
2023-05-02 11:18:56 +0530

    Update authContext.js

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -9,12 +9,12 @@ export const AuthContexProvider = ({ children }) => {
   );
 
   const login = async (inputs) => {
-    const res = await axios.post("/auth/login", inputs);
+    const res = await axios.post("https://blog-website-server-henna.vercel.app/api/auth/login", inputs);
     setCurrentUser(res.data);
   };
 
   const logout = async (nevigate) => {
-    await axios.post("/auth/logout");
+    await axios.post("https://blog-website-server-henna.vercel.app/api/auth/logout");
     setCurrentUser(null);
     nevigate('/login')
   };
